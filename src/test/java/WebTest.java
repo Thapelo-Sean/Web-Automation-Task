85c2b63241c8db120f7115c17a956f5a6e72e41b
Thapelo-Sean <thapelomatji12@gmail.com>
2024-04-18 11:18:51 +0200

    Modified WebTest.java file

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -45,13 +45,17 @@ public class WebTest {
     @Test(priority = 1)
     public void navigateToUrl(){
 
-        // Navigate to base url
-        driver.get(baseUrl);
-        driver.manage().window().maximize();
-        // Set an implicit wait of 10 seconds
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        // Set a page load timeout of 15 seconds
-        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+        try{
+            // Navigate to base url
+            driver.get(baseUrl);
+            driver.manage().window().maximize();
+            // Set an implicit wait of 10 seconds
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            // Set a page load timeout of 15 seconds
+            driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         // Create a test in the extent report and log the result
         extent.createTest("Navigate to Web Tables")
@@ -63,11 +67,15 @@ public class WebTest {
     @Test(priority = 2)
     public void validateUserListTable () {
 
-        // Get the actual title of the page
-        actualTitle = driver.getTitle();
+        try{
+            // Get the actual title of the page
+            actualTitle = driver.getTitle();
 
-        // Check if the actual title matches the expected title
-        Assert.assertEquals(actualTitle, expectedTitle);
+            // Check if the actual title matches the expected title
+            Assert.assertEquals(actualTitle, expectedTitle);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         // Create a test in the extent report and log the validation result
         extent.createTest("validate User List Table")
@@ -78,9 +86,12 @@ public class WebTest {
     @Test(priority = 3)
     public void clickAddUser() {
 
-        // Find the "Add User" button using XPath and click on it
-        driver.findElement(By.xpath("/html/body/table/thead/tr[2]/td/button")).click();
-
+        try{
+            // Find the "Add User" button using XPath and click on it
+            driver.findElement(By.xpath("/html/body/table/thead/tr[2]/td/button")).click();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         // Create a test with the name "Click add user"
         extent.createTest("Click add user")
                 .assignAuthor("Thapelo Matji")
@@ -91,30 +102,34 @@ public class WebTest {
     @Test(priority = 4)
     public void addUser1() {
 
-        // Locate elements and Fill in user details
-        driver.findElement(By.name("FirstName")).sendKeys("FName1");
-        driver.findElement(By.name("LastName")).sendKeys("LName1");
-        driver.findElement(By.name("UserName")).sendKeys("User1");
-        driver.findElement(By.name("Password")).sendKeys("Pass1");
-        driver.findElement(By.cssSelector("input[type='radio'][value='15']")).click();
+        try{
+            // Locate elements and Fill in user details
+            driver.findElement(By.name("FirstName")).sendKeys("FName1");
+            driver.findElement(By.name("LastName")).sendKeys("LName1");
+            driver.findElement(By.name("UserName")).sendKeys("User1");
+            driver.findElement(By.name("Password")).sendKeys("Pass1");
+            driver.findElement(By.cssSelector("input[type='radio'][value='15']")).click();
 
-        // Find the dropdown element by its name
-        WebElement dropdown = driver.findElement(By.name("RoleId"));
+            // Find the dropdown element by its name
+            WebElement dropdown = driver.findElement(By.name("RoleId"));
 
-        // Click on the dropdown to open the options
-        dropdown.click();
+            // Click on the dropdown to open the options
+            dropdown.click();
 
-        // Find all the options within the dropdown
-        List<WebElement> options = driver.findElements(By.cssSelector("select[name='RoleId'] option"));
+            // Find all the options within the dropdown
+            List<WebElement> options = driver.findElements(By.cssSelector("select[name='RoleId'] option"));
 
-        // Select the last option by its index and click
-        WebElement lastOption = options.get(options.size() - 1);
-        lastOption.click();
+            // Select the last option by its index and click
+            WebElement lastOption = options.get(options.size() - 1);
+            lastOption.click();
 
-        //Locate elements and Fill in user details
-        driver.findElement(By.name("Email")).sendKeys("admin@mail.com");
-        driver.findElement(By.name("Mobilephone")).sendKeys("082555");
-        driver.findElement(By.xpath("/html/body/div[2]/div[3]/button[2]")).click();
+            //Locate elements and Fill in user details
+            driver.findElement(By.name("Email")).sendKeys("admin@mail.com");
+            driver.findElement(By.name("Mobilephone")).sendKeys("082555");
+            driver.findElement(By.xpath("/html/body/div[2]/div[3]/button[2]")).click();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         //Log the test result using ExtentReports
         extent.createTest("Add user1")
@@ -126,52 +141,57 @@ public class WebTest {
     @Test(priority = 5)
     public void addUser2(){
 
-        //Locate the element and click the button to add a new user
-        driver.findElement(By.xpath("/html/body/table/thead/tr[2]/td/button")).click();
+        try{
 
-        //Locate elements, clear the input field and Fill in user2 details
-        WebElement firstName = driver.findElement(By.name("FirstName"));
-        firstName.clear();
-        firstName.sendKeys("FName2");
+            //Locate the element and click the button to add a new user
+            driver.findElement(By.xpath("/html/body/table/thead/tr[2]/td/button")).click();
 
-        WebElement lastName = driver.findElement(By.name("LastName"));
-        lastName.clear();
-        lastName.sendKeys("LName2");
+            //Locate elements, clear the input field and Fill in user2 details
+            WebElement firstName = driver.findElement(By.name("FirstName"));
+            firstName.clear();
+            firstName.sendKeys("FName2");
 
-        WebElement userName = driver.findElement(By.name("UserName"));
-        userName.clear();
-        userName.sendKeys("User2");
+            WebElement lastName = driver.findElement(By.name("LastName"));
+            lastName.clear();
+            lastName.sendKeys("LName2");
 
-        WebElement password = driver.findElement(By.name("Password"));
-        password.clear();
-        password.sendKeys("Pass2");
+            WebElement userName = driver.findElement(By.name("UserName"));
+            userName.clear();
+            userName.sendKeys("User2");
 
-        // Select the role as Customer from the dropdown
-        driver.findElement(By.cssSelector("input[type='radio'][value='16']")).click();
+            WebElement password = driver.findElement(By.name("Password"));
+            password.clear();
+            password.sendKeys("Pass2");
 
-        WebElement dropdown = driver.findElement(By.xpath("/html/body/div[2]/div[2]/form/table/tbody/tr[6]/td[2]/select"));
+            // Select the role as Customer from the dropdown
+            driver.findElement(By.cssSelector("input[type='radio'][value='16']")).click();
 
-        // Find the dropdown element by its name
-        dropdown = driver.findElement(By.xpath("/html/body/div[2]/div[2]/form/table/tbody/tr[6]/td[2]/select"));
+            WebElement dropdown = driver.findElement(By.xpath("/html/body/div[2]/div[2]/form/table/tbody/tr[6]/td[2]/select"));
 
-        // Click on the dropdown to open the options
-        dropdown.click();
+            // Find the dropdown element by its name
+            dropdown = driver.findElement(By.xpath("/html/body/div[2]/div[2]/form/table/tbody/tr[6]/td[2]/select"));
 
-        // Select the second option "Customer" by its index (index starts from 0)
-        WebElement secondOption = driver.findElements(By.cssSelector("select[name='RoleId'] option")).get(1);
-        secondOption.click();
+            // Click on the dropdown to open the options
+            dropdown.click();
 
-        //Locate elements, clear the input field and Fill in user2 details
-        WebElement email = driver.findElement(By.name("Email"));
-        email.clear();
-        email.sendKeys("cusomter@mail.com");
+            // Select the second option "Customer" by its index (index starts from 0)
+            WebElement secondOption = driver.findElements(By.cssSelector("select[name='RoleId'] option")).get(1);
+            secondOption.click();
 
-        WebElement mobilephone = driver.findElement(By.name("Mobilephone"));
-        mobilephone.clear();
-        mobilephone.sendKeys("083333");
+            //Locate elements, clear the input field and Fill in user2 details
+            WebElement email = driver.findElement(By.name("Email"));
+            email.clear();
+            email.sendKeys("cusomter@mail.com");
 
-        //Click on the Save button
-        driver.findElement(By.xpath("/html/body/div[2]/div[3]/button[2]")).click();
+            WebElement mobilephone = driver.findElement(By.name("Mobilephone"));
+            mobilephone.clear();
+            mobilephone.sendKeys("083333");
+
+            //Click on the Save button
+            driver.findElement(By.xpath("/html/body/div[2]/div[3]/button[2]")).click();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         //Log the test result using ExtentReports
         extent.createTest("Add user2")
@@ -182,8 +202,12 @@ public class WebTest {
     @Test(priority = 6)
     public void verifyUsername() {
 
-        // Ensure that the usernames for user1 and user2 are different
-        Assert.assertNotEquals(user1, user2, "Usernames for user1 and user2 should be different");
+        try{
+            // Ensure that the usernames for user1 and user2 are different
+            Assert.assertNotEquals(user1, user2, "Usernames for user1 and user2 should be different");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         // Log the test result using ExtentReports
         extent.createTest("Verify usernames are unique")
@@ -194,30 +218,35 @@ public class WebTest {
 
     @Test(priority = 7)
     public void verifyUsers() {
-        // Locate search input field perform a search in the table for the added user
-        WebElement searchField = driver.findElement(By.xpath("/html/body/table/thead/tr[1]/td/input"));
 
-        // Enter the username in the search field
-        searchField.sendKeys(username);
+        try{
+            // Locate search input field perform a search in the table for the added user
+            WebElement searchField = driver.findElement(By.xpath("/html/body/table/thead/tr[1]/td/input"));
 
-        // Submit the search
-        searchField.sendKeys(Keys.ENTER);
+            // Enter the username in the search field
+            searchField.sendKeys(username);
 
-        // Locate elements of both users
-        WebElement user1 = driver.findElement(By.xpath("//td[text()='User1']"));
-        WebElement user2 = driver.findElement(By.xpath("/html/body/table/tbody/tr[1]/td[3]"));
+            // Submit the search
+            searchField.sendKeys(Keys.ENTER);
 
-        // Check if both User1 and User2 elements are visible
-        boolean isUser1Visible = user1.isDisplayed();
-        boolean isUser2Visible = user2.isDisplayed();
+            // Locate elements of both users
+            WebElement user1 = driver.findElement(By.xpath("//td[text()='User1']"));
+            WebElement user2 = driver.findElement(By.xpath("/html/body/table/tbody/tr[1]/td[3]"));
 
-        // Check if both users are visible
-        if (isUser1Visible && isUser2Visible) {
-            Assert.assertTrue(true, "User1 is visible on the page.");
-            Assert.assertTrue(true, "User2 is visible on the page.");
-        } else {
-            Assert.assertFalse(isUser1Visible, "User1 is not visible on the page.");
-            Assert.assertFalse(isUser2Visible, "User2 is not visible on the page.");
+            // Check if both User1 and User2 elements are visible
+            boolean isUser1Visible = user1.isDisplayed();
+            boolean isUser2Visible = user2.isDisplayed();
+
+            // Check if both users are visible
+            if (isUser1Visible && isUser2Visible) {
+                Assert.assertTrue(true, "User1 is visible on the page.");
+                Assert.assertTrue(true, "User2 is visible on the page.");
+            } else {
+                Assert.assertFalse(isUser1Visible, "User1 is not visible on the page.");
+                Assert.assertFalse(isUser2Visible, "User2 is not visible on the page.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
         // Log the test result using ExtentReports
